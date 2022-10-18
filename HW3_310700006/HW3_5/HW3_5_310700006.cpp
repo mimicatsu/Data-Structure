a84d775c64c133dc637ea220a0fbe3a2fa0163f1
mimicatsu <mimicatsu5168@gmail.com>
2022-10-18 21:29:11 +0800

    finished HW3_5

--- HW3_310700006/HW3_5/HW3_5_310700006.cpp
+++ HW3_310700006/HW3_5/HW3_5_310700006.cpp
@@ -2,55 +2,13 @@
 using namespace std;
 #include <fstream>
 #include <string>
+#include <vector>
 
-struct stackNode{
-    int x;
-    int y;
-    stackNode *next;
-    stackNode(int a = 0, int b = 0, stackNode *n = nullptr);
-};
+using mazeType = vector<vector<int>>;
 
-stackNode::stackNode(int a, int b, stackNode *n)
-    :x{a},y{b},next{nullptr}
-    {}
-
-struct stackList{
-    stackNode *top;
-    stackList( stackNode *t = nullptr);
-    void push(int x, int y);
-    void pop();
-    bool isEmpty();
-};
-
-stackList::stackList(stackNode *t)
-    :top{t}
-    {}
-
-void stackList::push(int x, int y){
-    stackNode *newNode = new stackNode(x,y);
-    newNode->next = top;
-    top = newNode;
-}
-
-void stackList::pop(){
-    stackNode *tmp = top->next;
-    delete top;
-    top = tmp;
-}
-
-bool stackList::isEmpty(){
-    if(top == nullptr){
-        return true;
-    }
-    else return false;
-}
-
-
-
+void find(int row, int col, mazeType& maze, int end_x, int end_y, int maze_row, int maze_col);
 
 int main(){
-    stackList First;
-    stackList Second;
 
     string fileName = "input.txt";
     ifstream inputFile(fileName);
@@ -74,7 +32,7 @@ int main(){
     getline(inputFile, line,'\n');
     int end_y = stoi(line);
 
-    int maze[maze_row][maze_col];
+    mazeType maze(maze_row,vector<int>(maze_col,0));
 
     for( int i = 0; i < maze_row; i++){
         for ( int j = 0; j < maze_col; j ++){
@@ -82,15 +40,48 @@ int main(){
         }
     }
 
+
     inputFile.close();
-
-    for( int i = 0; i < maze_row; i++){
-        for ( int j = 0; j < maze_col; j ++){
-            cout << maze[i][j] << " "; 
-        }
-        cout << "\n";
-    }
-
+    find(start_x, start_y, maze, end_x, end_y, maze_row, maze_col);
+    
 
     return 0;
+}
+
+void find(int row, int col, mazeType& maze, int end_x, int end_y, int maze_row, int maze_col){
+    int i = row;
+    int j = col;
+    maze[i][j] = 2;
+    if(i == end_x && j == end_y){
+       
+        fstream file;
+        file.open("answer.txt",ios::out);
+        if(!file){
+            cout << "not open.";
+            exit(1);
+        }
+        for( int r = 0; r < maze_row; r++){
+            file << "\n";
+            for ( int c = 0; c < maze_col; c++){
+                file << maze[r][c] << " ";
+            }
+        }
+
+    }
+    if(i-1 >= 0 && maze[i-1][j] == 0  ){
+        maze[i-1][j] = 2;
+        find(i-1, j, maze, end_x, end_y, maze_row, maze_col);
+    }
+    if(i+1 <= end_x && maze[i+1][j] == 0){
+        maze[i+1][j] = 2;
+        find(i+1, j, maze, end_x, end_y, maze_row, maze_col);
+    }
+    if(j-1 >= 0 && maze[i][j-1] == 0){
+        maze[i][j-1] = 2;
+        find(i, j-1, maze, end_x, end_y, maze_row, maze_col);
+    }
+    if(j+1 <= end_y && maze[i][j+1] == 0){
+        maze[i][j+1] = 2;
+        find(i, j+1, maze, end_x, end_y, maze_row, maze_col);
+    }
 }
